158bfe1d8a645337eadd3ce1aedbd3ff260a2baa
Fith Wahome <wahomeimani@gmail.com>
2023-09-26 15:33:24 +0300

    assignment

--- index.js
+++ index.js
@@ -1,1 +1,54 @@
 // Write your solution here!
+
+// Write your solution here!
+const cats = ["Milo","Otis","Garfield"];
+beforeEach(function () {
+cats.length = 0;
+cats.push('Milo', 'Otis', 'Garfield');
+});
+function destructivelyAppendCat(){
+cats.push("Ralph");
+console.log(cats);
+}
+function destructivelyPrependCat(){
+cats.unshift("Bob");
+console.log(cats);
+}
+function destructivelyRemoveLastCat(){
+cats.pop();
+console.log(cats);
+}
+function destructivelyRemoveFirstCat(){
+cats.shift();
+console.log(cats);
+}
+function appendCat(){
+const newCatsArray = [...cats, "Broom"];
+return newCatsArray;
+}
+function prependCat(){
+let newCatsArrays = ["Arnold",...cats];
+return newCatsArrays;
+}
+//ts
+function removeLastCat() {
+if (cats.length === 0) {
+return [];
+}
+const newCatsArray = cats.slice(0, -1);
+return newCatsArray;
+}
+const newCatsArray = removeLastCat();
+console.log(newCatsArray); 
+console.log(cats); 
+//yd
+function removeFirstCat(){
+
+if( cats.length === 0){
+return[];
+}
+const newCatsArrays = cats.slice(1);
+return newCatsArrays;
+}
+console.log(newCatsArrays);
+console.log(cats);
